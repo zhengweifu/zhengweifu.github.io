0e83aeb152b606360f1d512142b46f98d25a1eaa
zhengweifu_win <zhengweifu@bitmap3d.com.cn>
2017-08-22 16:42:41 +0800

    Mod: some

--- js/components/Work/ThreeMakePage.js
+++ js/components/Work/ThreeMakePage.js
@@ -8,6 +8,6 @@ import { WEB_ROOT } from '../../config';
 
 export default () => {
 	return <WorkPage siderSelectedKey='1'  breadcrunbs={['工作', '三维制作']}>
-		<CommonRenderRelative relativePathName={`${WEB_ROOT}images/threeMake/`} relativeFileName='relative.json' />;
+		<CommonRenderRelative relativePathName={`${WEB_ROOT}images/threeMake/`} relativeFileName='relative.json' />
 	</WorkPage>
 };
